2d84e4568e23eae98e4daed01d8cb810851d533a
Garima <garima89077@gmail.com>
2022-07-08 17:49:28 +0530

    commit

---
+++ PrintSumRowWise.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printSum(int arr[][4],int row,int col)
+{
+    for(int row=0; row<3; row++) {
+        int sum=0;
+        for(int col=0; col<4; col++) {
+            sum+=arr[row][col];
+        }
+        cout<<sum<<" ";
+    }
+
+    
+}
+int main()
+{
+    int arr[3][4];
+    //put according to rows and columns
+    //int arr[3][4]={{1,11,111,111},{2,22,222,2222},{3,33,333,333}}
+    
+     cout << "Enter the elements " <<  endl;
+    //taking input -> row wise input
+    for(int row=0; row<3; row++) {
+        for(int col=0; col<4; col++) {
+            cin >> arr[row][col];
+        }
+    }
+    
+    cout << "Printing the array " << endl;
+    //print 
+        for(int row=0; row<3; row++) {
+        for(int col=0; col<4; col++) {
+            cout << arr[row][col] << " ";
+        }
+        cout << endl;
+    }
+    cout<<"sum"<<endl;
+   printSum(arr,3,4);
+    return 0;
+}
