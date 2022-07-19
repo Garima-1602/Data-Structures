426669f66fcac5937e1219a798a914e4e208374b
Garima <garima89077@gmail.com>
2022-07-19 11:42:08 +0530

    Updated

--- DoublePointer.cpp
+++ DoublePointer.cpp
@@ -1,7 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+int f(int x, int *py, int **ppz) {
+    int y, z;
+    **ppz += 1;
+    z = **ppz;
+    *py += 2;
+    y = *py;
+    x += 3;
+    return x + y + z;
+}
 int main()
-{
+{ /*
     int i=5;
     int* p=&i;
     int** p2=&p;
@@ -11,6 +20,15 @@ int main()
     cout<<*p<<endl; //value
     cout<<**p2<<endl; //value
 
-
+//address at p
+cout<<&i<<endl;
+cout<<p<<endl;
+cout<<&p2<<endl; */
+    int c, *b, **a;
+    c = 4;
+    b = &c;
+    a = &b;
+    cout << f(c, b, a);
+   
     return 0;
 }
---
+++ DynamicAllocationOf2DARRAY.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int **arr=new int*[n];
+    //creation
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=new int[n];
+    }
+    //taking input
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }cout<<endl;
+    //printing 
+     for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+    }
+    return 0;
+}
