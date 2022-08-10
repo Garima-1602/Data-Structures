9cebafd981d3f859be8d138c339650d181f55e30
Garima <garima89077@gmail.com>
2022-08-10 07:25:27 +0530

    initial

--- class.cpp
+++ class.cpp
@@ -2,12 +2,49 @@
 using namespace std;
 class Hero{
 //properties
+//by deafault these are private
+public:
 int health;
+char level;
+void print(){
+    cout<<level<<endl;
+}
+//getter
+int getHealth()
+{
+    return health;
+}
+char getLevel(){
+    return level;
+}
+//setter
+void setHealth(int h)
+{
+    health=h;
+}
+void setLevel(char ch)
+{
+    level=ch;
+}
 };
 int main()
 {
     //creation of object
     Hero h1;
-    cout<<"Size: "<<sizeof(h1)<<endl;
+    //h1.health=70;
+    //to set value
+    h1.setHealth(60);
+    h1.level='A';
+    //cout<<"Size: "<<sizeof(h1)<<endl;
+    cout<<"Health: "<<h1.getHealth()<<endl;
+    cout<<"Level: "<<h1.level<<endl;
+    //dynamic allocation
+    Hero *b=new Hero;
+    cout<<"Level: "<<(*b).level<<endl;
+    cout<<"Health : "<<(*b).getHealth()<<endl;
+
+     cout<<"Level: "<<b->level<<endl;
+    cout<<"Health : "<<b->getHealth()<<endl;
+
     return 0;
 }
