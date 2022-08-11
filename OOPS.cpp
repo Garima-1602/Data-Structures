9f3c318e185a1d264b13c6e796f1c3ad1f889f9b
Garima <garima89077@gmail.com>
2022-08-11 18:50:34 +0530

    initial

---
+++ OOPS.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+   private:
+   string name;
+   int age;
+   int height;
+
+   public:
+   int getAge()
+   {
+    return this->age;
+   }
+};
+int main()
+{
+    Student first;
+    cout<<"well"<<endl;
+   return 0;
+}
