37d9a7234370217a2c805b0f8e2c2c1e732cf848
Garima <garima89077@gmail.com>
2022-08-12 07:48:13 +0530

    initial

--- OOPS.cpp
+++ OOPS.cpp
@@ -1,20 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Student{
-   private:
-   string name;
-   int age;
+//encapsulation
+class Human{
+   public:
    int height;
-
+   int weight;
+   int age;
    public:
    int getAge()
    {
     return this->age;
    }
+   int setWeight(int w)
+   {
+       this->weight=w;
+   }
+};
+class Male:public Human
+{
+public:
+string color;
+void sleep()
+{
+    cout<<"Male sleep";
+}
 };
 int main()
 {
-    Student first;
-    cout<<"well"<<endl;
+    Male ob1;
+    cout<<ob1.age<<endl;//male inherit human
+    cout<<ob1.weight<<endl;
+    cout<<ob1.height<<endl;
+    cout<<ob1.color<<endl;
+    ob1.sleep();
    return 0;
 }
