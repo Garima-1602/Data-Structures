c2b111d97433c360739d10840d02098f57eeb256
Garima <garima89077@gmail.com>
2022-08-22 18:37:28 +0530

    initial

--- SinglyLinkedlist.cpp
+++ SinglyLinkedlist.cpp
@@ -4,9 +4,17 @@ class Node{
      public:
      int data;
      Node* next;
+     //constructor in linked list
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
 };
 int main()
 {
-    Node* node1=new Node();
+    Node* node1=new Node(10);
+    cout<<node1->data<<endl;
+    cout<<node1->next<<endl;
     return 0;
 }
