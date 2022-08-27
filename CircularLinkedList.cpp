27a07bdd7e2758b4ac73a7b09eeca22e1adb7ba9
Garima <garima89077@gmail.com>
2022-08-28 00:20:59 +0530

    initial

---
+++ CircularLinkedList.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+     public:
+     int data;
+     Node* next;
+     Node(int d)
+     {
+        this->data=d;
+        this->next=NULL;
+     }
+     ~Node(){
+     int val=this->data;
+        if(this->next!=NULL)
+        {
+            delete next;
+            next=NULL;
+        }
+        cout<<"Memory is free for "<<val<<endl;
+     }
+};
+void insertNode(Node* &tails,int element,int d)
+{
+    //assuming that the element is present in the list
+    if(tail==NULL)
+    {
+        Node* newNode=new Node(d);
+        tail=newNode;
+        newnode->next=newNode;
+    }
+    else{
+        //non empty list
+        //assuming that the element is present in the list
+        Node* curr=tail;
+        while(curr->data!=element)
+        {
+            curr=curr->next;
+        }
+        Node* temp=new Node(d);
+        temp->next=curr->next;
+        curr->next=temp;
+    }
+}
+void print(Node* tail)
+{
+    Node* temp=tail;
+    do{
+          cout<<tail->data<<" ";
+          tail=tail->next;
+    }while(tail!=temp);
+    cout<<endl;
+}
+int main()
+{
+    Node* tail=NULL;
+    //empty list insertion
+    insertNode(tail,5,3);
+    print(tail);
+    insertNode(tail,3,5);
+    print(tail);
+    insertNode(tail,5,7);
+    print(tail);
+    insertNode(tail,7,9);
+    print(tail);
+    insertNode(tail,5,6);
+    print(tail);
+    insertNode(tail,9,10);
+    print(tail);
+    insertNode(tail,3,4);
+    print(tail);
+    return 0;
+}
