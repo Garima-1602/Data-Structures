156043dde55bea31e339df3051c96b27d322fc91
Garima <garima89077@gmail.com>
2022-08-27 17:36:00 +0530

    initial

--- SinglyLinkedlist.cpp
+++ SinglyLinkedlist.cpp
@@ -10,6 +10,16 @@ class Node{
         this->data=data;
         this->next=NULL;
     }
+    ~Node()
+    {
+        int value=this->data;
+        if(this->next!=NULL)
+        {
+            delete next;
+            this->next=NULL;
+        }
+        cout<<"Memory is free for the value "<<value<<endl;
+    }
 };
 //reference is created to avoid copy of that and make changes in original list only
 void insertAtHead(Node* &head,int d){
@@ -52,6 +62,34 @@ void insertAtPosition(Node* &tail,Node* &head,int position,int d)
     nodeToInsert->next=temp->next;
     temp->next=nodeToInsert;
 }
+//deletion
+void deleteNode(int position,Node* &head)
+{
+    //deleting first or start node
+    if(position==1)
+    {
+        Node* temp=head;
+        head=head->next;
+        //memory free for start node
+        temp->next=NULL;
+        delete temp;
+    }
+    else{
+          //deleting any middle node or last node
+          Node* curr=head;
+          Node* prev=NULL;
+          int cnt=1;
+          while(cnt<=position)
+          {
+            prev=curr;
+            curr=curr->next;
+            cnt++;
+          }
+          prev->next=curr->next;
+          curr->next=NULL;
+          delete curr;
+    }
+}
 //how to traverse linked list
 void print(Node* &head)
 {
@@ -83,5 +121,7 @@ int main()
  //to verify tail and head
  cout<<"head "<<head->data<<endl;
  cout<<"tail "<<tail->data<<endl;
+ deleteNode(1,head);
+ print(head);
     return 0;
 }
