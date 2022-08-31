7a5ec25815e412e872bfa6043444155de5ad6687
Garima <garima89077@gmail.com>
2022-08-31 23:36:19 +0530

    initial

--- CircularLinkedList.cpp
+++ CircularLinkedList.cpp
@@ -126,6 +126,40 @@ bool detectLoop(Node* head)
     }
     return 0;
 }
+Node* floydDetectLoop(Node* head)
+{
+    if(head==NULL)
+     return NULL;
+    Node* slow=head;
+    Node* fast=head;
+    while(slow!=NULL &&fast!=NULL)
+    {
+        fast=fast->next;
+        if(fast!=NULL)
+        {
+            fast=fast->next;
+        }
+        slow=slow->next;
+        if(slow==fast)
+        {
+            return slow;
+        }
+    }
+    return NULL;
+}
+Node* getStartNode(Node* head)
+{
+    if(head==NULL)
+      return NULL;
+    Node* intersection=floydDetectLoop(head);
+    Node* slow=head;
+    while(slow!=intersection)
+    {
+        slow=slow->next;
+        intersection=intersection->next;
+    }
+    return slow;
+}
 int main()
 {
     Node* tail=NULL;
