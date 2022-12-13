6e951bb5d863266305bdd95875a516be72719d94
Garima <garima89077@gmail.com>
2022-12-13 18:43:36 +0530

    initial

---
+++ Deque.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    deque<int>q;
+    q.push_front(12);
+    q.push_back(14);
+    cout<<q.front()<<endl;
+    cout<<q.back()<<endl;
+    q.pop_front();//den 12 will be remove
+ cout<<q.front()<<endl;
+    cout<<q.back()<<endl;
+    return 0;
+
+}
