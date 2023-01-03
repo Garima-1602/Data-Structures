78b8bdb3e65e03f54eed42cdfa662cf95ea0349a
Garima <garima89077@gmail.com>
2023-01-03 11:51:27 +0530

    initial

---
+++ PriorityQueue.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //understanding basic implementation of priority queue
+    priority_queue<int>q;
+    //push element
+    q.push(10);
+    q.push(5);
+    q.push(100);
+    q.push(80);
+    q.push(200);
+    q.push(12);
+    cout<<"Top: "<<q.top()<<endl;
+    while(!q.empty())
+    {
+        cout<<q.top()<<endl;
+        q.pop();
+    }
+    return 0;
+}
