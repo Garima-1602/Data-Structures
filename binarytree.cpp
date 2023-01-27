68ec2d64791795a896f92e9193b9b83fd503c1b6
Garima <garima89077@gmail.com>
2023-01-27 22:57:30 +0530

    level order traversal

--- binarytree.cpp
+++ binarytree.cpp
@@ -34,12 +34,21 @@ void levelOrderTraversal(node* root)
 {
     queue<node*>q;
     q.push(root);
+    q.push(NULL); //print level wise
     while(!q.empty())
     {
         node* temp=q.front();
-        cout<<temp->data<<" ";
         q.pop();
-
+        if(temp==NULL)//previous level has been traversed 
+        {
+           cout<<endl;
+           if(!q.empty()) //child are present
+           {
+            q.push(NULL);
+           }
+        }
+        else{
+        cout<<temp->data<<" ";
         if(temp->left)
         {
             q.push(temp->left);
@@ -48,14 +57,34 @@ void levelOrderTraversal(node* root)
         {
             q.push(temp->right);
         }
-    }
+        }
+        
+    } 
 }
+//inorder traversal
+void inorder(node* root)
+{
+    //FOLLOW LNR
+    //Base case
+    if(root==NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
 int main()
 {
     node* root=NULL;
     root=buildTree(root);
     //1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1 
     //level order traversal
+    cout<<"Printing level order traversal: "<<endl;
     levelOrderTraversal(root);
+    cout<<"Inorder Traversal: ";
+    inorder(root);
+    
     return 0;
 }
