57b6991ab0f512545ff41a133e3c2a46bccb67d3
Garima <garima89077@gmail.com>
2023-02-14 19:24:27 +0530

    Zigzag tree traversal

--- binarytree.cpp
+++ binarytree.cpp
@@ -99,10 +99,40 @@ void preorder(node* root)
     preorder(root->right);
     
 }
+void buildTreeFromLevel(node* &root)
+{
+    queue<node*>q;
+    cout<<"Enter data for root "<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    q.push(root);
+    while(!q.empty())
+    {
+        node* temp=q.front();
+        q.pop();
+        cout<<"Enter left node for: "<<temp->data<<endl;
+        int leftData;
+        cin>>leftData;
+        if(leftData!=-1)
+        {
+            temp->left=new node(leftData);
+            q.push(temp->left);
+        }
+        cout<<"Enter right node for: "<<temp->data<<endl;
+        int rightData;
+        cin>>rightData;
+        if(rightData!=-1)
+        {
+            temp->right=new node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
 int main()
 {
     node* root=NULL;
-    root=buildTree(root);
+    /*root=buildTree(root);
     //1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1 
     //level order traversal
     cout<<"Printing level order traversal: "<<endl;
@@ -112,6 +142,8 @@ int main()
     cout<<"Postorder Traversal: "<<endl;
     postorder(root);
     cout<<"Preorder Traversal: "<<endl;
-    preorder(root);
+    preorder(root); */
+    buildTreeFromLevel(root);
+    levelOrderTraversal(root);
     return 0;
 }
